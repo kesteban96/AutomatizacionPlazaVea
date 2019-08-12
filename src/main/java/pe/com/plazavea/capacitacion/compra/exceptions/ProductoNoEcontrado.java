b371b25233c6593a8c08ba946f169dcc52dfcfb6
kesteban96 <kevingutierrezudea1996@gmail.com>
2019-08-12 02:31:55 -0500

    Delete ProductoNoEcontrado.java

--- src/main/java/pe/com/plazavea/capacitacion/compra/exceptions/ProductoNoEcontrado.java
+++
@@ -1,9 +0,0 @@
-package pe.com.plazavea.capacitacion.compra.exceptions;
-
-public class ProductoNoEcontrado extends AssertionError{
-
-	public ProductoNoEcontrado(String mensage, Throwable causa) {
-		super(mensage, causa);
-	}
-	
-}
